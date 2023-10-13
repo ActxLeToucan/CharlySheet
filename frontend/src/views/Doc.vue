0e291e26fa271463d3f4e4cb133416bc1c0275f5
FurWaz <fur.waz06@gmail.com>
2023-10-13 11:49:00 +0200

    Added debug infos in doc view

--- frontend/src/views/Doc.vue
+++ frontend/src/views/Doc.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="flex grow flex-col min-h-full h-full max-w-full w-full">
-        mode : {{ docMode == MODE_EDIT ? "Edit" : "New" }}
+        <p>mode : {{ docMode == MODE_EDIT ? "Edit" : "New" }}</p>
+        <p v-if="docMode === MODE_EDIT">
+            doc : id={{ docId }}
+        </p>
     </div>
 </template>
 
@@ -15,7 +18,8 @@ export default {
         return {
             MODE_NEW: 0,
             MODE_EDIT: 1,
-            docMode: (this.$route.params.id === "new" ? 0 : 1)
+            docMode: (this.$route.params.id === "new" ? 0 : 1),
+            docId: this.$route.params.id
         };
     },
     mounted() {
