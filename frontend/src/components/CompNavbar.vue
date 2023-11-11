9ff3a8cf285cb6c673dab3abc7371baf2df53b4b
FurWaz <fur.waz06@gmail.com>
2023-11-11 17:49:35 +0100

    Improved /my page

--- frontend/src/components/CompNavbar.vue
+++ frontend/src/components/CompNavbar.vue
@@ -1,23 +1,20 @@
 <template>
     <div class="show-down flex h-fit border-b-2 border-slate-800 p-2">
         <div class="flex items-center w-[30%]">
-            <!-- <button
-                class="text-indigo-600 dark:text-indigo-400 p-1.5 rounded-lg hover:bg-slate-200 hover:dark:bg-slate-600 hover:text-slate-700 hover:dark:text-slate-200 transition-all"
-                @click="() => $router.go(-1)"
-            >
-                <chevron-left-icon class="w-6 md:w-8" />
-            </button> -->
+            <comp-icon class="w-10" />
         </div>
         <div class="flex items-center mx-auto">
-            <comp-title>CharlySheet</comp-title>
+            <comp-title>
+                <get-text :context="Lang.CreateTranslationContext('my', 'Documents')" />
+            </comp-title>
         </div>
         <div class="flex items-center w-[30%] justify-end">
-            <button
-                class="text-indigo-600 dark:text-indigo-400 p-1.5 rounded-lg hover:bg-slate-200 hover:dark:bg-slate-600 hover:text-slate-700 hover:dark:text-slate-200 transition-all"
-                @click="disconnect"
-            >
-                <user-icon class="w-6 md:w-8" />
-            </button>
+            <comp-button
+                :icon="UserMinusIcon"
+                :onclick="disconnect"
+            >  
+                <get-text :context="Lang.CreateTranslationContext('verbs', 'LogOut')" />
+            </comp-button>
         </div>
     </div>
 </template>
@@ -26,21 +23,28 @@
 import CompTitle from './CompTitle.vue'
 
 import {
-    UserIcon,
+    UserMinusIcon,
     // ChevronLeftIcon
 } from '@heroicons/vue/24/outline';
 import User from '../scripts/User';
+import CompButton from './CompButton.vue';
+import GetText from './text/GetText.vue';
+import Lang from '../scripts/Lang';
+import CompIcon from './CompIcon.vue';
 
 export default {
     name: "CompNavbar",
     components: {
         CompTitle,
-        UserIcon,
+        CompButton,
+        GetText,
+        CompIcon,
         // ChevronLeftIcon
     },
     data() {
         return {
-            
+            Lang,
+            UserMinusIcon
         };
     },
     mounted() {
