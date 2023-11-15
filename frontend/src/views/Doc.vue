d957104d9f3fc075ceab062c038a40616fe11cf7
FurWaz <fur.waz06@gmail.com>
2023-11-15 11:17:33 +0100

    WIP top bar options and cell selection

--- frontend/src/views/Doc.vue
+++ frontend/src/views/Doc.vue
@@ -4,21 +4,24 @@
             <div class="flex w-fit h-full items-center justify-center pr-2">
                 <document-icon class="show-right w-14 h-full text-slate-500 dark:text-slate-400 bg-slate-200 dark:bg-slate-800 rounded-lg p-1" />
             </div>
-            <div class="flex flex-col w-full h-full">
+            <div class="flex flex-col w-full h-full space-y-2">
                 <div class="flex w-full h-fit">
                     <div class="show-right">
                         <comp-input value="Nouveau document" />
                     </div>
-                    <div>
-                        <p class="hidden">
-                            uwu <!-- CONTEXT MENU / TODO -->
-                        </p>
+                    <div class="flex space-x-4 px-4 items-center justify-center">
+                        <button
+                            v-for="menu in menus"
+                            :key="menu.name"
+                            class="flex items-center justify-center w-fit py-1 px-2 rounded-md text-slate-700 dark:text-slate-200
+                                   hover:bg-slate-200 hover:dark:bg-slate-600 transition-all"
+                        >
+                            {{ menu.name }}
+                        </button>
                     </div>
                 </div>
-                <div class="w-full h-8">
-                    <p class="hidden">
-                        owo <!-- FORMULA BAR + FORMATING / TODO -->
-                    </p>
+                <div class="w-fit h-fit">
+                    <comp-input label="Formule" />
                 </div>
             </div>
         </div>
@@ -62,7 +65,7 @@
                     </div>
                     <div
                         ref="grid-container"
-                        class="flex grow w-full h-full overflow-auto bg-slate-50 dark:bg-slate-700"
+                        class="flex grow relative w-full h-full overflow-auto bg-slate-50 dark:bg-slate-700"
                     >
                         <div class="w-fit h-fit">
                             <div
@@ -95,6 +98,17 @@ import {
 } from '@heroicons/vue/24/outline';
 import CompInput from '../components/CompInput.vue';
 
+const menus = [
+    {name: 'Fichier'},
+    {name: 'Edition'},
+    {name: 'Affichage'},
+    {name: 'Insertion'},
+    {name: 'Format'},
+    {name: 'Outils'},
+    {name: 'Données'},
+    {name: 'Aide'}
+];
+
 export default {
     name: "DocView",
     components: {
@@ -110,6 +124,7 @@ export default {
             docId: this.$route.params.id,
             nbRows: 30,
             nbCols: 19,
+            menus
         };
     },
     mounted() {
@@ -123,6 +138,25 @@ export default {
         container.addEventListener('scroll', ev => {
             rowBar.scrollTo(0, ev.target.scrollTop);
             colBar.scrollTo(ev.target.scrollLeft, 0);
+        });
+
+        let isMouseDown = false;
+        let startDiv = null;
+        container.addEventListener('mousedown', ev => {
+            isMouseDown = true;
+            startDiv = ev.target;
+            this.setBorder(startDiv);
+        });
+        container.addEventListener('mouseup', ev => { isMouseDown = false; });
+        container.addEventListener('keydown', ev => {
+            if (ev.key === 'Escape') {
+                document.querySelector(".border-selector")?.remove();
+            }
+        });
+        container.addEventListener('mousemove', ev => {
+            if (isMouseDown) {
+                this.setBorder(startDiv, ev.target);
+            }
         });
 
         window.colBar = colBar;
@@ -141,6 +175,38 @@ export default {
         },
         getSlotAt(x, y) {
             return new Slot();
+        },
+        setBorder(dom1, dom2) {
+            const dom1rect = dom1?.getBoundingClientRect();
+            const dom2rect = dom2?.getBoundingClientRect();
+            /**@type {HTMLDivElement} */
+            const container = this.$refs['grid-container'];
+            const containerRect = container.getBoundingClientRect();
+
+            const x = (dom2
+                ? Math.min(dom1rect.x, dom2rect.x)
+                : dom1rect.x) - containerRect.x;
+            const y = (dom2
+                ? Math.min(dom1rect.y, dom2rect.y)
+                : dom1rect.y) - containerRect.y;
+            const w = dom2
+                ? Math.max(dom1rect.x, dom2rect.x) - Math.min(dom1rect.x, dom2rect.x) + dom2rect.width
+                : dom1rect.width;
+            const h = dom2
+                ? Math.max(dom1rect.y, dom2rect.y) - Math.min(dom1rect.y, dom2rect.y) + dom2rect.height
+                : dom1rect.height;
+
+            let border = document.querySelector(".border-selector");
+            if (!border) {
+                border = document.createElement('div');
+                border.className = "border-selector";
+                container.firstElementChild.appendChild(border);
+            }
+            border.style.position = 'absolute';
+            border.style.left = x + 'px';
+            border.style.top = y + 'px';
+            border.style.width = w + 'px';
+            border.style.height = h + 'px';
         }
     },
     meta: {
@@ -151,3 +217,9 @@ export default {
 }
 
 </script>
+
+<style>
+.border-selector {
+    @apply bg-indigo-500/[0.1] border-2 border-indigo-500 rounded pointer-events-none;
+}
+</style>
