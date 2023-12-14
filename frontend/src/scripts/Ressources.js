a00249dea342eb33b3432d6e4d998f5dc0412b17
FurWaz <fur.waz06@gmail.com>
2023-12-14 14:17:12 +0100

    Added ressources caching and smart fetching

---
+++ frontend/src/scripts/Ressources.js
@@ -0,0 +1,37 @@
+import CachedRessource from "./CachedRessource";
+import API from "./API";
+import User from "../models/User";
+
+export default class Ressources {
+    static #USERS_FETCHER = id => async () => {
+        const res = await API.execute_logged(API.ROUTE.USERS(id));
+        return new User(res);
+    };
+
+    static #users = {};
+
+    /**
+     * Returns the ressource corresponding to the given id in the given cache.
+     * If the ressource is not in the cache, it will be fetched from the server,
+     * and added to the cache.
+     * @param {string} id Ressource id
+     * @param {object} cache Cache object to search in
+     * @param {function} fetcher Function to call to fetch the ressource
+     * @returns The ressource
+     */
+    static async #getRessource(id, cache, fetcher) {
+        if (!cache[id]) {
+            cache[id] = new CachedRessource(fetcher);
+        }
+        return await cache[id].get();
+    }
+
+    /**
+     * Returns the user corresponding to the given id
+     * @param {string} id User id
+     * @returns The user
+     */
+    static async getUser(id) {
+        return await Ressources.#getRessource(id, Ressources.#users, Ressources.#USERS_FETCHER(id));
+    }
+}
